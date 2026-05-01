77e72ee645355b9b59877ea3a4aaac52a1145e83
Noah Smith <noah@statespace.dev>
2026-05-01 14:19:57 +0200

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM ubuntu:24.04
 RUN apt-get update && apt-get install build-essential cmake telnet -y
-ADD external /external
-WORKDIR /external
-RUN gcc back10.c -o back10
-#RUN ./back10 -l -f ./t20v4/tapes/BB-5254D-BM.tap -T e11
+
 
--- dec-20/read_t20anv4_tape.py
+++ dec-20/read_t20anv4_tape.py
@@ -1,7 +1,7 @@
 import os
 for line in open('filenames.txt', 'rt'):
     filename = line.strip().split('>')[1]
-    os.system(f'./back10mac -f ./t20v4/tapes/BB-5254D-BM.tap -T e11 -x {filename}')
+    os.system(f'./back10mac -f ./BB-5254D-BM.tap -T e11 -x {filename}')
     if 'system' in line: os.system(f'mv ps\:\<new-system\>{filename} ./t20anv4/system/{filename}')
     elif 'subsys' in line: os.system(f'mv ps\:\<new-subsys\>{filename} ./t20anv4/subsys/{filename}')
     elif 'uetp' in line: os.system(f'mv ps\:\<uetp.lib\>{filename} ./t20anv4/uetp/{filename}')
